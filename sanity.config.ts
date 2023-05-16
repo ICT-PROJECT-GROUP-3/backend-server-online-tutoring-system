91a8b55d5c0dc98e58b1ac0d3533b1e5132daae9
Themba100 <bsc-com-09-18@unima.ac.mw>
2023-05-16 02:41:34 +0200

    changed code

--- sanity.config.ts
+++ sanity.config.ts
@@ -9,14 +9,7 @@ export default defineConfig({
   title: 'online tutoring system',
   projectId: '3iouolde',
   dataset: 'production',
-<<<<<<< HEAD
   plugins: [deskTool(), visionTool(),muxInput()],
-=======
-  plugins: [
-     deskTool(),
-     visionTool(),
-     muxInput({mp4_support: 'standard'})],
->>>>>>> 210bb2801df6794157d5b94077db8ac21ca1ecfe
   schema: {
     types: schemaTypes,
   },
